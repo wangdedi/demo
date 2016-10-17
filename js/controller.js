12969fc2ff5d77d7d83492277b2ca5b9bdc61a19
wangdedi <546026340@qq.com>
2016-10-17 15:42:45 +0800

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -7,7 +7,10 @@ angular.module("app.controller1",[])
 .controller("territoryCtrl",function ($scope) {
 	$scope.datas=tourismData1;
 })
-
+angular.module("app.controller3",[])
+.controller("abroadCtrl",function ($scope) {
+	$scope.datas=tourismData1;
+})
 angular.module("app.controller2",[])
 .controller("detailsCtrl",['$stateParams',function($stateParams){
     console.log($stateParams.questionId);
